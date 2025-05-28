bf7f7a869c6a48fc98af66ef9c34414a76613e36
deepugowda-git <deepthibh0808@gmail.com>
2025-05-28 11:07:28 +0530

    updating

--- src/app.js
+++ src/app.js
@@ -2,20 +2,30 @@ const express=require('express');
 
 const app=express();
 
-// app.use("/",(req,res)=>{
-//     res.send("Hello I starting my server at /")
-// })
+// This will handle only get call to /user
+app.get('/user',(req,res)=>{
+res.send({firstName:'deepthi',lastName:'H'})
+})
 
+ 
+app.post('/user',(req,res)=>{
+     console.log('The maruthi data successfully sended');
+     
+    res.send('i can able to fetch the data successfully')
+})
+
+// IF we use it will match all the http method api calls to /test
 app.use("/hello",(req,res)=>{
     res.send('How are You hemanth!  Hope you are Doing GOOD');
     
 })
 
 
-app.use("",(req,res)=>{
+app.use("/user",(req,res)=>{
     res.send("Hello,Hemanth!")
+ })
 
-})
+
 
 app.listen(8008,()=>{
     console.log("The server is successfully listening on the port 8008...");
