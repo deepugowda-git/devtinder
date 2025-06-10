cccb2555369e630fa3ef7efe4422ae699bc456ca
deepugowda-git <deepthibh0808@gmail.com>
2025-06-10 11:25:40 +0530

    encrypt password

--- src/app.js
+++ src/app.js
@@ -2,20 +2,54 @@ const express = require('express');
 const connectDB = require('./config/database');
 const User = require('./models/user');
 const app = express();
+const {validateSignUp}=require('./utils/validate');
+const bcrypt=require('bcrypt');
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const user = new User(req.body);
+   
     try {
+         //validating data email and password
+         validateSignUp(req);
+
+        const {firstName,lastName,emailId, password}=req.body;
+         //Encrypt password
+        const passwordHash= await bcrypt.hash(password,10);
+        console.log(passwordHash);
+         //creating new instance of user model
+         const user = new User({
+            firstName,
+            lastName,
+            password:passwordHash, 
+            emailId
+         });
         await user.save();
         res.send("User data added successfully...");
     } catch (err) {
-        res.status(400).send("Error saving the data: " + err.message);
+        res.status(400).send("Error : " + err.message);
     }
 });
 
+app.post("/login", async (req,res)=>{
+    try{
+      const {emailId,password}=req.body;
+      const user=await User.findOne({emailId:emailId})
+      if(!user) {
+        throw new Error("invalid credentials");
+        
+      }
+      const isPasswordValid=await bcrypt.compare(password,user.password)
+      if(!isPasswordValid){
+        throw new Error("password not valid");
+      }else {
+        res.send("login successfully")
+      }
 
+    }catch(err){
+        res.status(400).send("ERROR:"+ err.message)
+    }
+})
 //get user by email
 
 app.get("/user",async(req,res)=>{
@@ -87,9 +121,6 @@ app.patch("/user/:userId",async(req,res)=>{
          res.status(400).send("update failed:"+ err.message)
     }
 })
-
-
-
 
 connectDB()
     .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,66 +1,62 @@
-const mongoose=require('mongoose');
-const validator=require('validator');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema=new mongoose.Schema({
-    firstName:{
-      type:  String,
-      required:true,
-      minLength:4,
-      maxLength:50
-    },
-    lastName:{
-      type:  String
-    },
-    emailId:{
-      type:String,
-      lowercase:true,
-      required:true,
-      unique:true,
-      trim:true,
-      validate(value){
-        if(!validator.isEmail(value)){
-          throw new Error("invalid email address:"+value);
-          
-        }
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50
+  },
+  lastName: {
+    type: String
+  },
+  emailId: {
+    type: String,
+    lowercase: true,
+    required: true,
+    unique: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("invalid email address: " + value);
       }
-    },
-        
-    password:{
-         type:String,
-         required:true,
-          validate(value){
-        if(!validator.isStrongPassword(value)){
-          throw new Error("enter strong password:"+value);
-          
-        }
-        },
-        age:{
-            type:Number,
-            min:18,
-        },
-        gender:{
-            type:String,
-            validate(value){
-              if(!["male","female","others"].includes(value)){
-                throw new Error("gender data is not valid")
-              }
-            }
-        },
-        photoUrl:{
-          type:String,
-          default:"string"
-        },
-        about:{
-          type:String,
-          default:"this is a default value of the user"
-        },
-        skills:{
-          type:[String],
-          
-        }
-    
-}},{
-  timestamps:true,
-})
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("enter strong password: " + value);
+      }
+    }
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("gender data is not valid");
+      }
+    }
+  },
+  photoUrl: {
+    type: String,
+    default: "string"
+  },
+  about: {
+    type: String,
+    default: "this is a default value of the user"
+  },
+  skills: {
+    type: [String],
+  }
+}, {
+  timestamps: true,
+});
 
-module.exports=mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
